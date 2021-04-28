157adbac1ef33092958f8dea848584bde5c9567c
jooo21 <wndus24088@naver.com>
2021-04-29 04:50:17 +0900

    communityend

--- src/sub/community/community.js
+++ src/sub/community/community.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-    
-})
+    var $panel = $("#panel");
+    var input = $panel.find("td>p");
+
+    $(window).on("resize ready",function(){
+        if($(window).width() <1179) { 
+            var inputText = input.text().substr(0,30) + "...";
+            input.text(inputText);
+         }
+         if($(window).width() <539) { 
+            var inputText = input.text().substr(0,20) + "...";
+            input.text(inputText);
+         }
+    });
+});
