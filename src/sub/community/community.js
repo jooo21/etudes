f955b025f18702382449d40ae4f762e0b797abeb
jooo21 <wndus24088@naver.com>
2021-04-29 16:31:08 +0900

    faqend

--- src/sub/community/community.js
+++ src/sub/community/community.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
     var $panel = $("#panel");
     var input = $panel.find("td>p");
 
-    $(window).on("resize ready",function(){
+    $(window).on("resize load",function(){
         if($(window).width() <1179) { 
             var inputText = input.text().substr(0,30) + "...";
             input.text(inputText);
          }
          if($(window).width() <539) { 
-            var inputText = input.text().substr(0,20) + "...";
+            var inputText = input.text().substr(0,15) + "...";
             input.text(inputText);
          }
     });
